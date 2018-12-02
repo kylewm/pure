c11324424ea86a071dd7d2dedeeaad979d04f73d
Lyra Naeseth <lyra@naeseth.com>
2018-12-01 19:38:07 -0800

    Handle untracked files correctly

--- pure.zsh
+++ pure.zsh
@@ -256,9 +256,12 @@ prompt_pure_async_git_status() {
 	local file_status status_summary untracked unstaged staged
 
 	git status --porcelain --ignore-submodules -unormal | while IFS='' read -r file_status; do
-		[[ "${file_status:0:2}" == '??' ]] && untracked='%F{red}●'
-		[[ "${file_status:0:1}" != ' ' ]] && staged='%F{green}●'
-		[[ "${file_status:1:2}" != ' ' ]] && unstaged='%F{yellow}●'
+		if [[ "${file_status:0:2}" == '??' ]]; then
+			untracked='%F{red}●'
+		else
+			[[ "${file_status:0:1}" != ' ' ]] && staged='%F{green}●'
+			[[ "${file_status:1:2}" != ' ' ]] && unstaged='%F{yellow}●'
+		fi
 	done
 
 	status_summary="${untracked}${unstaged}${staged}"
